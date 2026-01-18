08cc19c02ebb812ba50115d782820a66ac014ddb
Radelja <jkjbknesw@gmail.com>
2026-01-18 22:41:16 +0100

    najjakiji svić kejsovi

--- src/cpp/Selection.cpp
+++ src/cpp/Selection.cpp
@@ -1,2 +1,147 @@
 #include "../header/Selection.h"
 
+Selection::Selection() = default;
+
+Selection::~Selection();
+
+void Selection::setPos(Solitaire::PileType pT, int pI = 0, int cI = 0){
+    area = pT;
+    pileIndex = pI;
+    cardIndex = cI;
+}
+
+void Selection::moveRight(){
+    switch(getArea()){
+    case Solitaire::PileType::D{
+        setArea(Solitaire::PileType::W);
+        break;
+    }
+    case Solitaire::PileType::W{
+        setArea(Solitaire::PileType::F);
+        setPileIndex(0);
+        break;
+    }
+    case Solitaire::PileType::F{
+        if(getPileIndex()>3){
+            setArea(Solitaire::PileType::T);
+            setPileIndex(0);
+            setCardIndex(0);
+        }
+        break;
+    }
+    case Solitaire::PileType::T{
+        if(getPileIndex()<6){
+            setPileIndex(getPileIndex()+1);
+            break;
+        }
+    }
+    }
+}
+
+void Selection::moveLeft(){
+    switch(getArea()){
+    case Solitaire::PileType::T{
+        if(getPileIndex()==0){
+            setArea(Solitaire::PileType::F);
+            setPileIndex(3);
+        }else{
+            setArea(getArea()-1);
+        }
+        break;
+    }
+    case Solitaire::PileType::F{
+        if(getPileIndex()==0){
+            setArea(Solitaire::PileType::W);
+        }else{
+            setPileIndex(getPileIndex()-1);
+        }
+        break;
+    }
+    case Solitaire::PileType::W{
+        setArea(Solitaire::PileType::D);
+        break;
+    }
+    default{
+        break;
+    }
+    }
+}
+
+void Selection::moveUp(){
+    if(getArea()==Solitaire::PileType::T && getCardIndex() > 0){
+        setCardIndex(getCardIndex()-1);
+    }else if(getCardIndex() == 0){
+        switch(getPileIndex()){
+        case(0){
+            setArea(Solitaire::PileType::D);
+        }
+        case(1){
+            setArea(Solitaire::PileType::D);
+        }
+        case(2){
+            setArea(Solitaire::PileType::D);
+        }
+        case(3){
+            setArea(Solitaire::PileType::W);
+        }
+        case(4){
+            setArea(Solitaire::PileType::F);
+            setPileIndex(0);
+        }
+        case(5){
+            setArea(Solitaire::PileType::F);
+            setPileIndex(0);
+        }
+        case(6){
+            setArea(Solitaire::PileType::F);
+            setPileIndex(0);
+        }
+        }
+    }
+}
+
+void Selection::moveDown(){
+    switch(getArea()){
+    case(Solitaire::PileType::D){
+        setArea(Solitaire::PileType::T);
+        setPileIndex(0);
+        setCardIndex(0);
+    }
+    case(Solitaire::PileType::W){
+        setArea(Solitaire::PileType::T);
+        setPileIndex(3);
+        setCardIndex(0);
+    }
+    case(Solitaire::PileType::F){
+        switch(getPileIndex()){
+        case(0){
+            setArea(Solitaire::PileType::T);
+            setPileIndex(4);
+            setCardIndex(0);
+        }
+        case(1){
+            setArea(Solitaire::PileType::T);
+            setPileIndex(5);
+            setCardIndex(0);
+        }
+        case(2){
+            setArea(Solitaire::PileType::T);
+            setPileIndex(6);
+            setCardIndex(0);
+        }
+        case(3){
+            setArea(Solitaire::PileType::T);
+            setPileIndex(6);
+            setCardIndex(0);
+        }
+        }
+    }
+    case(Solitaire::PileType::T){
+        if(getCardIndex() => TablePile::getPileSize()){
+            setCardIndex(TablePile::getPileSize());
+        }else{
+            setCardIndex(getCardIndex()+1);
+        }
+    }
+    }
+}
--- src/header/Selection.h
+++ src/header/Selection.h
@@ -2,16 +2,26 @@
 #define SELECTION_H
 
 #include "Types.h"
+#include "TablePile.h"
 
 class Selection {
 private:
-	Solitaire::PileType area;
-
-	int pile_index = 0;
-	int card_index = 0;
+	Solitaire::PileType area=Solitaire::PileType::T;
+	int pileIndex = 0;
+	int cardIndex = 0;
 	bool holding = false;
 public:
-
+    inline int getPileIndex() const {return pileIndex};
+    inline void setPileIndex(int pI) {pileIndex = pI};
+    inline int getCardIndex() const {return cardIndex};
+    inline void setCardIndex(int cI) const {cardIndex = cI};
+    inline Solitaire::PileType getArea() const {return area};
+    inline void setArea(Solitaire::PileType pT) {area=pT};
+    void moveRight();
+    void moveLeft();
+    void moveUp();
+    void moveDown();
+    void pickCard();
 };
 
 #endif
